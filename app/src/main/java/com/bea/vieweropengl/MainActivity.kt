42f364590ead52dd79ce547fa9ce4a51d89e566b
Abhishek Chaudhary <acha@bea.be>
2021-07-02 11:25:21 +0530

    first Commit

---
+++ app/src/main/java/com/bea/vieweropengl/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.bea.vieweropengl
+
+import android.opengl.GLSurfaceView
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var gLView: GLSurfaceView
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Create a GLSurfaceView instance and set it as the ContentView for this Activity.
+        gLView = MyGLSurfaceView(this)
+        setContentView(gLView)
+    }
+}
