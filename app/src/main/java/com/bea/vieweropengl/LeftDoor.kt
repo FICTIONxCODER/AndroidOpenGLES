f78adb14dca156e51dd80f042d87de49f1e32ca8
Abhishek Chaudhary <acha@bea.be>
2021-07-29 16:23:34 +0530

    GL_DrawArrays instead of GL_DrawElements.

--- app/src/main/java/com/bea/vieweropengl/LeftDoor.kt
+++ app/src/main/java/com/bea/vieweropengl/LeftDoor.kt
@@ -15,49 +15,95 @@ class LeftDoor {
     // Buffer for index-array
     private var indexBuffer  : ByteBuffer? = null
 
-    /*private val colors = arrayOf(floatArrayOf(0.5f,0.7f,0.5f,0.7f),
-            floatArrayOf(0.5f,0.7f,0.5f,0.7f),
-            floatArrayOf(0.5f,0.7f,0.5f,0.7f),
-            floatArrayOf(0.5f,0.7f,0.5f,0.7f),
-            floatArrayOf(0.5f,0.7f,0.5f,0.7f),
-            floatArrayOf(0.5f,0.7f,0.5f,0.7f))*/
+    /*private val colors = arrayOf(floatArrayOf(0.5f,0.7f,0.5f,0.5f),
+            floatArrayOf(0.5f,0.7f,0.5f,0.5f),
+            floatArrayOf(0.5f,0.7f,0.5f,0.5f),
+            floatArrayOf(0.5f,0.7f,0.5f,0.5f),
+            floatArrayOf(0.5f,0.7f,0.5f,0.5f),
+            floatArrayOf(0.5f,0.7f,0.5f,0.5f))*/
 
     // Vertices of the 6 faces
     private fun SideScreenSafetyCoordinates(xMin:Float, yMin:Float, zMin:Float, xMax:Float, yMax:Float, zMax:Float):FloatArray {
 
         val vertices = mutableListOf<Float>()
-        vertices.add(xMin)					//Left bottom front
+        //FRONT
+        vertices.add(xMin)		//Left bottom front
         vertices.add(yMin)
         vertices.add(zMax)
-
         vertices.add(xMax)		//Right bottom front
         vertices.add(yMin)
         vertices.add(zMax)
-
         vertices.add(xMin)		//Left Top front
         vertices.add(yMax)
         vertices.add(zMax)
-
-        vertices.add(xMax)					//Right Top front
+        vertices.add(xMax)		//Right Top front
         vertices.add(yMax)
         vertices.add(zMax)
-
-        vertices.add(xMin)					//Left bottom back
+        //BACK
+        vertices.add(xMin)	    //Left bottom back
         vertices.add(yMin)
         vertices.add(zMin)
-
         vertices.add(xMax)		//Right bottom back
         vertices.add(yMin)
         vertices.add(zMin)
-
         vertices.add(xMin)		//Left Top back
         vertices.add(yMax)
         vertices.add(zMin)
-
-        vertices.add(xMax)					//Right Top back
+        vertices.add(xMax)		//Right Top back
         vertices.add(yMax)
         vertices.add(zMin)
-
+        //LEFT
+        vertices.add(xMin)	    //Left bottom back
+        vertices.add(yMin)
+        vertices.add(zMin)
+        vertices.add(xMin)		//Left bottom front
+        vertices.add(yMin)
+        vertices.add(zMax)
+        vertices.add(xMin)		//Left Top back
+        vertices.add(yMax)
+        vertices.add(zMin)
+        vertices.add(xMin)		//Left Top front
+        vertices.add(yMax)
+        vertices.add(zMax)
+        // RIGHT
+        vertices.add(xMax)		//Right bottom front
+        vertices.add(yMin)
+        vertices.add(zMax)
+        vertices.add(xMax)		//Right bottom back
+        vertices.add(yMin)
+        vertices.add(zMin)
+        vertices.add(xMax)		//Right Top front
+        vertices.add(yMax)
+        vertices.add(zMax)
+        vertices.add(xMax)		//Right Top back
+        vertices.add(yMax)
+        vertices.add(zMin)
+        // TOP
+        vertices.add(xMin)		//Left Top front
+        vertices.add(yMax)
+        vertices.add(zMax)
+        vertices.add(xMax)		//Right Top front
+        vertices.add(yMax)
+        vertices.add(zMax)
+        vertices.add(xMin)		//Left Top back
+        vertices.add(yMax)
+        vertices.add(zMin)
+        vertices.add(xMax)		//Right Top back
+        vertices.add(yMax)
+        vertices.add(zMin)
+        // BOTTOM
+        vertices.add(xMin)	    //Left bottom back
+        vertices.add(yMin)
+        vertices.add(zMin)
+        vertices.add(xMax)		//Right bottom back
+        vertices.add(yMin)
+        vertices.add(zMin)
+        vertices.add(xMin)		//Left bottom front
+        vertices.add(yMin)
+        vertices.add(zMax)
+        vertices.add(xMax)		//Right bottom front
+        vertices.add(yMin)
+        vertices.add(zMax)
         return vertices.toFloatArray()
     }
     // Vertex indices of the 4 Triangles
@@ -74,10 +120,10 @@ class LeftDoor {
         vertexBuffer?.put(SideScreenSafetyCoordinates(xMin, yMin, zMin, xMax, yMax, zMax)) // Copy data into buffer
         vertexBuffer?.position(0) // Rewind
 
-        // Setup index-array buffer. Indices in byte.
+        /*// Setup index-array buffer. Indices in byte.
         indexBuffer = ByteBuffer.allocateDirect(indices.size)
         indexBuffer?.put(indices)
-        indexBuffer?.position(0)
+        indexBuffer?.position(0)*/
     }
 
     // Draw the shape
@@ -87,17 +133,19 @@ class LeftDoor {
         gl.glCullFace(GL10.GL_BACK) // Cull the back face (don't display)
         gl.glEnableClientState(GL10.GL_VERTEX_ARRAY)
         gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer)
-        GLES10.glEnable(GL10.GL_BLEND)
-        GLES10.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA)
 
+        GLES10.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA)
+        GLES10.glEnable(GL10.GL_BLEND)
 
         // Render all the faces
         for (face in 0 until numFaces) {
             // Set the color for each of the faces
             //gl.glColor4f(colors[face][0], colors[face][1], colors[face][2], colors[face][3])
-            gl.glColor4f(0.5f,0.7f,0.5f,0.7f)
+            gl.glColor4f(0.5f,0.7f,0.5f,0.5f)
             // Draw the primitive from the vertex-array directly
-            gl.glDrawElements(GL10.GL_TRIANGLE_STRIP, indices.size, GL10.GL_UNSIGNED_BYTE,indexBuffer)
+            //gl.glDrawElements(GL10.GL_TRIANGLE_STRIP, indices.size, GL10.GL_UNSIGNED_BYTE,indexBuffer)
+            //gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, indices.size, GL10.GL_UNSIGNED_BYTE)
+            gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, face * 4, 4)
         }
         gl.glDisableClientState(GL10.GL_VERTEX_ARRAY)
         gl.glDisable(GL10.GL_CULL_FACE)
