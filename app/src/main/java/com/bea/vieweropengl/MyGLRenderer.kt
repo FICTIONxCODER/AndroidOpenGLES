56cbceed3d14adbfbd5536ad6c74a76a45202a12
Abhishek Chaudhary <acha@bea.be>
2021-08-04 11:43:31 +0530

    Scanning cone updated according to device values.

--- app/src/main/java/com/bea/vieweropengl/MyGLRenderer.kt
+++ app/src/main/java/com/bea/vieweropengl/MyGLRenderer.kt
@@ -34,8 +34,8 @@ class MyGLRenderer : GLSurfaceView.Renderer {
     constructor(context: Context?) {
         this.context = context
 
-        quad = Square()
-        scanningCone = ScanningCone(-0.85f,0.0f,-0.45f,0.85f,2.20f,0.05f)
+        quad = Square(0.0f,2.20f,0f)
+        scanningCone = ScanningCone(0.0f,2.20f,0f)
         cube = Cube(-0.85f,0.0f,-0.45f,0.85f,2.20f,0.05f)
         leftdoor1 = LeftDoor(-1.70f,0.00f,-0.15f,-0.85f,2.20f,0.05f)
         leftdoor2 = LeftDoor(0.85f,0.00f,-0.15f,1.70f,2.20f,0.05f)
@@ -92,14 +92,13 @@ class MyGLRenderer : GLSurfaceView.Renderer {
         floor?.draw(gl)
         object1?.draw(gl)
         object2?.draw(gl)
-        quad!!.draw(gl) // Draw Sensor
+        quad!!.draw(gl,0.0f,2.20f,0f) // Draw Sensor
         scanArea?.draw(gl,-0.85f,0.0f,-0.45f,0.85f,2.20f,0.05f)
         virtualPushButton1?.draw(gl,1.00f,1.20f,-0.15f,1.20f,1.40f,0.05f)
-
         cube?.draw(gl)
         leftdoor1?.draw(gl)
         leftdoor2?.draw(gl)
-        scanningCone?.draw(gl,-0.85f,0.0f,-0.45f,0.85f,2.20f,0.05f)
+        scanningCone?.draw(gl,0.0f,2.20f,0f)
 
     }
 }
--- app/src/main/java/com/bea/vieweropengl/ScanningCone.kt
+++ app/src/main/java/com/bea/vieweropengl/ScanningCone.kt
@@ -13,15 +13,15 @@ class ScanningCone {
     // Buffer for vertex-array
     private var vertexBuffer : FloatBuffer? = null
 
-      fun scanSphereCoordinates(xMin: Float, yMin: Float, zMin: Float, xMax: Float, yMax: Float, zMax: Float):FloatArray {
+      fun scanSphereCoordinates(x: Float, y: Float, z: Float):FloatArray {
           //var mid:Double = (((xMax - xMin) / 2 - xMin).toDouble() - xMin).pow(2.0) + (((yMax - yMin) / 2 - yMin).toDouble() - yMin).pow(2.0)
           //var radius: Double = sqrt(mid)
           var radius: Double =1.0
           val vertices = mutableListOf<Float>()
           //CONE
-          vertices.add(0.0f)		//cone top points
-          vertices.add(2.20f)
-          vertices.add(0.0f)
+          vertices.add(x)		//cone top points
+          vertices.add(y)
+          vertices.add(z)
 
           for (i in 0..360) {
               vertices.add((radius * cos(i * Math.PI / 180f)).toFloat())      //X coordinate added
@@ -33,24 +33,24 @@ class ScanningCone {
       }
 
     // Constructor - Set up the buffers
-    constructor(xMin: Float, yMin: Float, zMin: Float, xMax: Float, yMax: Float, zMax: Float) {
+    constructor(x: Float, y: Float, z: Float) {
         // Setup vertex-array buffer. Vertices in float. An float has 4 bytes
-        val vbb: ByteBuffer = ByteBuffer.allocateDirect(scanSphereCoordinates(xMin, yMin, zMin, xMax, yMax, zMax).size * 4)
+        val vbb: ByteBuffer = ByteBuffer.allocateDirect(scanSphereCoordinates(x,y,z).size * 4)
         vbb.order(ByteOrder.nativeOrder()) // Use native byte order
         vertexBuffer = vbb.asFloatBuffer() // Convert from byte to float
-        vertexBuffer?.put(scanSphereCoordinates(xMin, yMin, zMin, xMax, yMax, zMax)) // Copy data into buffer
+        vertexBuffer?.put(scanSphereCoordinates(x,y,z)) // Copy data into buffer
         vertexBuffer?.position(0) // Rewind
 
 
     }
 
     // Draw the shape
-    fun draw(gl: GL10,xMin: Float, yMin: Float, zMin: Float, xMax: Float, yMax: Float, zMax: Float) {
+    fun draw(gl: GL10,x: Float, y: Float, z: Float) {
         gl.glEnableClientState(GL10.GL_VERTEX_ARRAY)
         gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer)
         gl.glColor4f(0.0f, 0.5f, 1.0f, 0.2f);      // Set the current color (NEW)
         // Draw the primitives from the vertex-array directly
-        gl.glDrawArrays(GL10.GL_TRIANGLE_FAN,  0,scanSphereCoordinates(xMin, yMin, zMin, xMax, yMax, zMax).size/3)
+        gl.glDrawArrays(GL10.GL_TRIANGLE_FAN,  0,scanSphereCoordinates(x,y,z).size/3)
         gl.glDisableClientState(GL10.GL_VERTEX_ARRAY)
     }
 }
